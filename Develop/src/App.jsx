c6f7af2d3734f0d045d9f60f30c1298eced6e3e0
LunarWyrm <englerprogramming@gmail.com>
2024-12-12 02:26:35 -0500

    Designed Sidebar
    - Imported Bulma for further design use
    - Styled sidebar
    - Made logo nondraggable

--- Develop/src/App.jsx
+++ Develop/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Correct imports
 import Home from "./pages/Home";
 import "./styles/App.css";
+import 'bulma/css/bulma.min.css';
 import TransactionList from "./pages/Transactions/TransactionList";
 import BudgetList from "./pages/Budget/BudgetList";
 import GroceryList from "./pages/Grocery/GroceryList";
@@ -11,28 +12,34 @@ function App() {
     <Router>
       <div className="App">
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/grocery-list">Grocery</Link>
-            </li>
-            <li>
-              <Link to="/budget-list">Budget</Link>
-            </li>
-            <li>
-              <Link to="/transaction-list">Transactions</Link>
-            </li>
-          </ul>
+          <img className="budgie-logo" src="src\assets\images\Budgie_Logo.png" alt="Budgie Logo" draggable="false" />
+          <div className="columns">
+            <ul class="menu">
+              <li class="menu-list">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="menu-list">
+                <Link to="/grocery-list">Grocery</Link>
+              </li>
+              <li class="menu-list">
+                <Link to="/budget-list">Budget</Link>
+              </li>
+              <li class="menu-list">
+                <Link to="/transaction-list">Transactions</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/grocery-list" element={<GroceryList />} />
-          <Route path="/budget-list" element={<BudgetList />} />
-          <Route path="/transaction-list" element={<TransactionList />} />
-        </Routes>
+        
+        {/* Content */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/grocery-list" element={<GroceryList />} />
+            <Route path="/budget-list" element={<BudgetList />} />
+            <Route path="/transaction-list" element={<TransactionList />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
